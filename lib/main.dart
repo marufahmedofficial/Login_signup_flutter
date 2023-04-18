0d299ad6364c710e1cd07be29d64cb7c1e3c9cfe
marufahmedofficial <mk6116055@gmail.com>
2023-04-18 17:07:46 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'SigIn SignUp Page Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
